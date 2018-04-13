6e9307174458392b7773bab3615c14ce78dc3516
1knowledge1 <iaroslaw.bondarenko@yandex.ru>
2018-04-13 23:51:19 +0300

    added

---
+++ sources/queue.cpp
@@ -0,0 +1,14 @@
+#include "queue_t.hpp"
+int main()
+{
+	queue_t<int> que;
+	que.push(1);
+	que.push(2);
+	que.push(3);
+	que.print(std::cout);
+	std::cout << que.pop();
+	std::cout << que.pop();
+	queue_t<int> que1 = que;
+	que1.print(std::cout);
+    return 0;
+}
