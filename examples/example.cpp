5167f6b3218d768f7c7cd3568d21f8babaaaeca2
1knowledge1 <iaroslaw.bondarenko@yandex.ru>
2018-05-14 16:24:56 +0300

    added

--- examples/example.cpp
+++ examples/example.cpp
@@ -2,6 +2,14 @@
 #include "Graph.hpp"
 
 int main() {
-	
+	Graph g;
+	if(g.read(std::cin))
+	{
+		g.DFS(2, std::cout);
+	}
+	else
+	{
+		std::cout << "error\n";
+	}
 	return 0;
 }
--- tests/graph_TEST.cpp
+++ tests/graph_TEST.cpp
@@ -1,4 +1,34 @@
 #include <catch.hpp>
 #include <sstream>
 #include "Graph.hpp"
+using namespace std;
 
+TEST_CASE(" read and DFS "){
+    Graph g;
+    string input {
+	"4\n"
+        "0 1 1 0\n"
+        "0 0 1 0\n"
+        "1 0 0 1\n"
+        "0 0 0 1\n"
+    };
+    istringstream stream (input);
+    g.read(stream);
+    string result = "2 0 1 3 ";
+    ostringstream output;
+    g.DFS(2, output);
+    REQUIRE(result == output.str());
+}
+
+TEST_CASE(" invalid input "){
+    Graph g;
+    string input {
+	"4\n"
+        "0 1 a 1\n"
+        "1 0 1 0\n"
+        "0 1 0 0\n"
+        "1 0 0 0\n"
+    };
+    istringstream stream (input);
+    REQUIRE(g.read(stream) == 0);
+}
