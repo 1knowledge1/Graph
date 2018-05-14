a12324030282e04daf85b5d43383ecabb21642b5
1knowledge1 <iaroslaw.bondarenko@yandex.ru>
2018-05-14 09:47:21 +0300

    init

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
-#include "tree_t.hpp"
+#include "Graph.hpp"
 
 int main() {
-	tree_t<int> tree;
-	char op;
-	while (std::cin >> op && op != 'q') {
-		int num;
-		switch (op) {
-		case '=':
-			tree.print(std::cout);
-			break;
-		case '+':
-			if (std::cin >> num) {
-				tree.insert(num);
-				tree.print(std::cout);
-			}
-			else {
-				std::cout << "An error has occured while reading input data\n";
-				exit(0);
-			}
-			break;
-		case '?':
-			if (std::cin >> num) {
-				if (tree.find(num)) {
-					std::cout << "true\n";
-				}
-				else {
-					std::cout << "false\n";
-				}
-			}
-			else {
-				std::cout << "An error has occured while reading input data\n";
-				exit(0);
-			}
-			break;
-		}
-	}
+	
 	return 0;
 }
---
+++ tests/graph_TEST.cpp
@@ -0,0 +1,4 @@
+#include <catch.hpp>
+#include <sstream>
+#include "Graph.hpp"
+
